0deaa6f79058d8e27c44f254d42a362586209aee
Simpo16002 <Simpo16002@Simpo16002-PC>
2016-03-29 15:10:49 +0800

    modify global message.properties

--- Fancy/src/main/java/cn/telling/config/Global.java
+++ Fancy/src/main/java/cn/telling/config/Global.java
@@ -37,7 +37,7 @@ public class Global {
 	/**
 	 * 属性文件加载对象
 	 */
-	private static PropertiesLoader loader = new PropertiesLoader("messages.properties");
+	private static PropertiesLoader loader = new PropertiesLoader("fancy.properties");
 
 	/**
 	 * 显示/隐藏
