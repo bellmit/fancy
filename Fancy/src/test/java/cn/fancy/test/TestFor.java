6d6a820aecb062dd5857ad7fb07ca49125058759
zhgo116 <419841083@qq.com>
2016-04-22 18:10:50 +0800

    commit testfor

--- Fancy/src/test/java/cn/fancy/test/TestFor.java
+++ Fancy/src/test/java/cn/fancy/test/TestFor.java
@@ -3,6 +3,8 @@ package cn.fancy.test;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.collect.Lists;
+
 public class TestFor {
 	public static void main(String[] args) {
 		List l = new ArrayList();
@@ -12,6 +14,15 @@ public class TestFor {
 		for (int i = 0, len = l.size(); i < len; i++) {
 		  System.out.println("=========测试");
 		}
-		
+	  System.out.println(l.size());
+	   Object[] elementData=new Object[10];
+	   elementData[0]=l;
+	   System.out.println(elementData.length);
+	   System.out.println(elementData[0]);
+	   List datas=Lists.newArrayList();
+	  System.out.println(datas.size());
+	  System.out.println("aSpring.java".matches(".*\\.java$"));
+	  System.out.println("123"+1);
+	  
 	}
 }
