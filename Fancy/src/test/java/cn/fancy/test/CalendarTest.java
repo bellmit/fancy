45bfcbc53c85a8d0dcf859fc9ebe740c65c59ef8
zhgo116 <419841083@qq.com>
2016-04-22 18:48:05 +0800

    remove repeat annotation

--- Fancy/src/test/java/cn/fancy/test/CalendarTest.java
+++ Fancy/src/test/java/cn/fancy/test/CalendarTest.java
@@ -1,22 +1,22 @@
 /*
-* Copyright (c) 2015年, SIMPO Technology. All Rights Reserved.
-* SIMPO Technology. CONFIDENTIAL
-*/
+ * Copyright (c) 2015年, SIMPO Technology. All Rights Reserved. SIMPO Technology. CONFIDENTIAL
+ */
 package cn.fancy.test;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;import com.sun.org.apache.bcel.internal.generic.CALOAD;
+import java.util.Calendar;
 
 /**
- * @description 
+ * @description
  * @author caosheng
  * @version 2016年4月22日
  */
 public class CalendarTest {
+
   public static void main(String[] args)
   {
-    Calendar c=Calendar.getInstance();
-    SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+    Calendar c = Calendar.getInstance();
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
     System.out.println(sdf.format(c.getTime()));
     System.out.println(c.get(Calendar.HOUR_OF_DAY));
   }
