5913de8ee7a264922c49303d8af3fdcead68566d
zhgo116 <419841083@qq.com>
2016-04-22 18:39:06 +0800

    add CalendarTest

---
+++ Fancy/src/test/java/cn/fancy/test/CalendarTest.java
@@ -0,0 +1,22 @@
+/*
+* Copyright (c) 2015年, SIMPO Technology. All Rights Reserved.
+* SIMPO Technology. CONFIDENTIAL
+*/
+package cn.fancy.test;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * @description 
+ * @author caosheng
+ * @version 2016年4月22日
+ */
+public class CalendarTest {
+  public static void main(String[] args)
+  {
+    Calendar c=Calendar.getInstance();
+    SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+    System.out.println(sdf.format(c.getTime()));
+  }
+}
