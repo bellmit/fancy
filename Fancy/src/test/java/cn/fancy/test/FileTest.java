047cf97c6df19fd42c833dbabbc693aca2d9519a
zhgo116 <419841083@qq.com>
2016-06-03 09:12:10 +0800

    test filetest和string operator

--- Fancy/src/test/java/cn/fancy/test/FileTest.java
+++ Fancy/src/test/java/cn/fancy/test/FileTest.java
@@ -1,8 +1,10 @@
 package cn.fancy.test;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigDecimal;
+
+import org.springframework.util.StringUtils;
+
 
 /**
  * @Title: FileTest.java
@@ -13,9 +15,24 @@ import java.io.IOException;
  * @version V1.0
  */
 public class FileTest {
-  public static void main(String[] args) throws IOException {
-    File file=new File("Test.txt");
-    file.createNewFile();
-    System.out.println( file.exists());
+
+  public static void main(String[] args) throws IOException
+  {
+    // 创建一个空文件
+    /*
+     * File file=new File("Test1.txt"); 
+     * file.createNewFile(); 
+     * System.out.println( file.exists());
+     */
+
+    BigDecimal b = new BigDecimal(12.1212);
+    System.out.println(b.subtract(new BigDecimal(10)));
+    Double c = new Double(-233.12);
+    System.out.println(c - (-233.12));
+   String a="abcdef,123";
+    StringBuilder sb=new StringBuilder("a").append("d").append("a");//推荐
+    
+    System.out.println(StringUtils.delimitedListToStringArray(a, ","));
+    
   }
 }
