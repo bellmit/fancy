fc516eb309aa36c60f741cea6d183b794e0d3f0e
zhgo116 <zhgo116@qq.com>
2016-08-30 22:49:15 +0800

    新增生产消费者例子

---
+++ Fancy/src/test/java/thread/ProducerConsumerDemo.java
@@ -0,0 +1,20 @@
+package thread;
+
+/**   
+ * @Title: ProducerConsumerDemo.java 
+ * @Package thread 
+ * @Description: 演示生成者消费者的例子
+ * @author 操圣
+ * @date 2016年8月30日 下午10:44:48 
+ * @version V1.0   
+ */
+public class ProducerConsumerDemo {
+
+	
+	class Resouce{
+		private String name;
+		private int counnt=1;
+	}
+}
+
+
